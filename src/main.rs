89644dac456933e01f93c197ae37b96ebb7d75f4
John Cole <john@johncole.nz>
2022-04-11 23:32:19 +1200

    Add Static scaffolding and use lowercase directory convention

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+#[macro_use] extern crate rocket;
+use rocket::serde::{Deserialize, Serialize};
+use rocket::serde::json::Json; 
+
+use rocket_contrib::serve::StaticFiles;
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", StaticFiles::from("/static"))
 }
